fcbebfc06b62deefd371cc83f0a6b05adff3c638
Vertaler <dancheran@yandex.ru>
2016-11-25 09:51:40 +0300

    color_test refctoring

--- tests/utils_test/color_test.py
+++ tests/utils_test/color_test.py
@@ -6,7 +6,7 @@ STUB_TEXT = "stub"
 from ..lib import case
 
 COLOR_CODES = {
-    "red":   '\033\[31m',
+    "red":   "\033\[31m",
     "blue":  "\033\[34m",
     "green":  "\033\[32m",
     "yellow": "\033\[33m",
@@ -17,23 +17,17 @@ class ColorCase(case.BaseTestCase):
     def get_regex(self, color):
         return re.compile( '^' + COLOR_CODES[color] + STUB_TEXT )
 
-    def checkYellow(self):
+    def test_yellow(self):
         self.assertRegexpMatches(colors.yellow(STUB_TEXT), self.get_regex("yellow"))
 
-    def checkGreen(self):
+    def test_green(self):
         self.assertRegexpMatches( colors.green(STUB_TEXT) , self.get_regex("green"))
 
-    def checkRed(self):
+    def test_red(self):
         self.assertRegexpMatches(colors.red(STUB_TEXT), self.get_regex("red"))
 
-    def checkBlue(self):
+    def test_blue(self):
         self.assertRegexpMatches(colors.blue(STUB_TEXT), self.get_regex("blue"))
-
-    def runTest(self):
-        self.checkBlue()
-        self.checkGreen()
-        self.checkRed()
-        self.checkYellow()
 
 
 if __name__ == '__main__':

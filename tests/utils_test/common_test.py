aac20051db739e2d2c737e423b38e888d09bc462
Vertaler <dancheran@yandex.ru>
2016-11-27 01:29:20 +0300

    Полное покрытие utils/common

--- tests/utils_test/common_test.py
+++ tests/utils_test/common_test.py
@@ -1,8 +1,10 @@
-import unittest
-from seismograph.utils import common
-from seismograph.exceptions import TimeoutException
-import mock
 import sys
+import unittest
+
+import mock
+
+from seismograph.exceptions import TimeoutException
+from seismograph.utils import common
 
 START_TIME_STUB = 1000
 END_TIME_STUB = 5000
@@ -12,6 +14,42 @@ TIMEOUT_STUB = 0.00001
 DELAY_STUB = 1
 CHAIN_LENGTH_STUB = 5
 PATH_STUB = 'PATH_STUB'
+LIST_OF_DICTS_STUB = [{'k1': 1, 'k2': 2}]
+CORRECT_PATTERN_STUB = {'k1': 1, 'k2': 2}
+INCORRECT_PATTERN_STUB = {'k1': 1, 'k2': 5}
+
+DICT1_STUB = {
+    'list': [{'k1': {'k2': 5, 'k3': 7}}, {'k1': 1}],
+    'dict': {'k1': {'k1': 5}, 'k2': {'k3': '1'}},
+    'val': 5,
+    'not_in_dict2': 1
+}
+DICT2_STUB = {
+    'list': [{'k1': 5}, {'k3': '1'}],
+    'dict': {'k1': {'k3': 5}},
+    'val': 4
+}
+REDUCE_DICT_RESULT = {
+    'list': [{}, {'k1': {'k2': 5, 'k3': 7}}],
+    'dict': {'k1': {}},
+    'val': 5
+}
+
+LIST1_STUB = [
+    [{'k1': {'k2': 5, 'k3': 7}}, {'k1': 1}],
+    {'k1': {'k1': 5}, 'k2': {'k3': '1'}},
+    5,
+]
+LIST2_STUB = [
+    [{'k1': 5}, {'k3': '1'}],
+    {'k1': {'k3': 5}},
+    4
+]
+REDUCE_LIST_RESULT = [
+    5,
+    {'k1': {}},
+    ([{}, {'k1': {'k3': 7, 'k2': 5}}], [{'k1': 5}, {'k3': '1'}]),
+]
 
 
 class CommonCase(unittest.TestCase):
@@ -36,16 +74,59 @@ class CommonCase(unittest.TestCase):
         common.call_to_chain(chain, 'mock_func')
         self.assertEqual(mock_obj.mock_func.call_count, len(chain))
 
+    def test_get_dict_from_list(self):
+        self.assertDictEqual(common.get_dict_from_list(LIST_OF_DICTS_STUB, **CORRECT_PATTERN_STUB),
+                             CORRECT_PATTERN_STUB)
+
+    def test_get_dict_from_list_without_pattern(self):
+        self.assertEqual(common.get_dict_from_list(LIST_OF_DICTS_STUB), LIST_OF_DICTS_STUB)
+
+    def test_get_fict_from_list_with_incorrect_pattern(self):
+        self.assertRaises(LookupError, common.get_dict_from_list, LIST_OF_DICTS_STUB, **INCORRECT_PATTERN_STUB)
+
+
+class DictReduceCase(unittest.TestCase):
+    def setUp(self):
+        self.dict1 = dict(DICT1_STUB)
+
+    def test_reduce_dict(self):
+        self.assertDictEqual(common.reduce_dict(self.dict1, DICT2_STUB), REDUCE_DICT_RESULT)
+
+    def test_with_different_lists_length(self):
+        self.assertRaises(AssertionError, common.reduce_dict, self.dict1, {'list': []})
+
+
+class ListReduceCase(unittest.TestCase):
+    def setUp(self):
+        self.list2 = list(LIST2_STUB)
+
+    def test_first_return_value(self):
+        result, _ = common.reduce_list(LIST1_STUB, self.list2)
+        self.assertListEqual(result, REDUCE_LIST_RESULT)
+
+    def test_second_return_value(self):
+        def deepsort(lst):
+            result = list(lst)
+            map(lambda l: deepsort(l) if isinstance(l, list) else l, result)
+            result.sort()
+            return result
+
+        expected_result = deepsort(self.list2)
+        _, result = common.reduce_list(LIST1_STUB, self.list2)
+        self.assertListEqual(result, expected_result)
+
+    def test_with_different_length(self):
+        self.assertRaises(AssertionError, common.reduce_list, LIST1_STUB, [])
+
+
 class PythonPathsCase(unittest.TestCase):
     def test_pythonpaths(self):
         common.pythonpaths(PATH_STUB)(lambda: 0)
         self.assertIn(PATH_STUB, sys.path)
 
     def tearDown(self):
-        try:
-            sys.path.remove(PATH_STUB)
-        except:
-            pass
+        sys.path.remove(PATH_STUB)
+
 
 class WaitingForCase(unittest.TestCase):
     def setUp(self):
@@ -93,35 +174,3 @@ class WaitingForCase(unittest.TestCase):
             common.waiting_for(self.mock_func, TIMEOUT_STUB, delay=DELAY_STUB)
         except:
             self.assertGreater(mock_sleep.call_count, 0)
-
-
-            # def waiting_for(func, timeout=None, exc_cls=None, message=None, delay=None, args=None, kwargs=None):
-            #     args = args or tuple()
-            #     kwargs = kwargs or dict()
-            #
-            #     timeout = timeout or 0
-            #     message = message or 'Timeout "{}" exceeded'.format(timeout)
-            #
-            #     if timeout:
-            #         t_start = time.time()
-            #
-            #         while time.time() <= t_start + timeout:
-            #             result = func(*args, **kwargs)
-            #
-            #             if result:
-            #                 return result
-            #
-            #             if delay:
-            #                 time.sleep(delay)
-            #         else:
-            #             if exc_cls:
-            #                 raise exc_cls(message)
-            #             raise TimeoutException(message)
-            #
-            #     result = func(*args, **kwargs)
-            #
-            #     if result:
-            #         return result
-            #     if exc_cls:
-            #         raise exc_cls(message)
-            #     raise TimeoutException(message)
